bac0c367855a5069af7725b14ce1c381aa808a83
Steven Xu <steven.shifu.xu@gmail.com>
2023-01-12 19:37:26 -0800

    Update Print-FooBar-Alternately.java

--- Print-FooBar-Alternately.java
+++ Print-FooBar-Alternately.java
@@ -42,3 +42,37 @@ class FooBar {
         }
     }
 }
+
+
+// easier solution with Semaphore
+class FooBar {
+    private int n;
+    private Semaphore semFoo;
+    private Semaphore semBar;
+
+    public FooBar(int n) {
+        this.n = n;
+        this.semFoo = new Semaphore(1);
+        this.semBar = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            semFoo.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            semBar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            semBar.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            semFoo.release();
+        }
+    }
+}
