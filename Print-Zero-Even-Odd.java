954a4db9c909259776f82a2e4386a32851852701
Steven Xu <steven.shifu.xu@gmail.com>
2023-01-12 20:09:35 -0800

    Update Print-Zero-Even-Odd.java

--- Print-Zero-Even-Odd.java
+++ Print-Zero-Even-Odd.java
@@ -68,3 +68,48 @@ class ZeroEvenOdd {
         }
     }
 }
+
+
+// easier solution with semaphore
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore sem0;
+    private Semaphore sem1;
+    private Semaphore sem2;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+        this.sem0 = new Semaphore(1);
+        this.sem1 = new Semaphore(0);
+        this.sem2 = new Semaphore(0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            sem0.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 1) {
+                sem1.release();
+            } else {
+                sem2.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            sem2.acquire();
+            printNumber.accept(i);
+            sem0.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            sem1.acquire();
+            printNumber.accept(i);
+            sem0.release();
+        }
+    }
+}
