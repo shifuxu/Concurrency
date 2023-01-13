1e76a06e034df3be4a1d7d866c1b88ff4962fd2a
Steven Xu <steven.shifu.xu@gmail.com>
2023-01-12 19:36:47 -0800

    Update Fizz-Buzz-Multithreaded.java

--- Fizz-Buzz-Multithreaded.java
+++ Fizz-Buzz-Multithreaded.java
@@ -87,3 +87,79 @@ class FizzBuzz {
         }
     }
 }
+
+
+// easier solution with Semaphore
+class FizzBuzz {
+    private int n;
+    private Semaphore fizz;
+    private Semaphore buzz;
+    private Semaphore fizzBuzz;
+    private Semaphore number;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+        // init the top 3 with 0 permits, so it will wait until been released by previous number.
+        fizz = new Semaphore(0);
+        buzz = new Semaphore(0);
+        fizzBuzz = new Semaphore(0);
+        number = new Semaphore(1);
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                fizz.acquire();
+                printFizz.run();
+                releaseLock(i + 1);
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 == 0) {
+                buzz.acquire();
+                printBuzz.run();
+                releaseLock(i + 1);
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                fizzBuzz.acquire();
+                printFizzBuzz.run();
+                releaseLock(i + 1);
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                number.acquire();
+                printNumber.accept(i);
+                releaseLock(i + 1);
+            }
+        }
+    }
+    
+    // key idea, release the lock with i + 1 to determine so that we can maintain the order.
+    private void releaseLock(int k) {
+        if (k % 3 == 0 && k % 5 != 0) {
+            fizz.release();
+        } else if (k % 3 != 0 && k % 5 == 0) {
+            buzz.release();
+        } else if (k % 3 == 0 && k % 5 == 0) {
+            fizzBuzz.release();
+        } else {
+            number.release();
+        }
+    }
+}
